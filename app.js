9fc3f6cca88060319be7857f5bf4dddb716a171f
kobe <youcode@skiff.com>
2023-10-26 10:47:13 +0100

    add javascript

---
+++ app.js
@@ -0,0 +1,15 @@
+document.getElementById("burger").addEventListener("click", function() {
+    const menu = document.getElementById("mobileMenu");
+    const burgerIcon = document.getElementById("burgerIcon");
+
+    if (menu.classList.contains("mobile-menu-active")) {
+        menu.classList.remove("mobile-menu-active");
+        burgerIcon.classList.remove("fa-x");
+        burgerIcon.classList.add("fa-bars");
+        
+    } else {
+        menu.classList.add("mobile-menu-active");
+        burgerIcon.classList.remove("fa-bars");
+        burgerIcon.classList.add("fa-x");
+    }
+});
